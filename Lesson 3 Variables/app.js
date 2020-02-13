4f8199fc00350971fd523a5c71d4b7a4d81d9097
Evan DeSousa <evandesousa@ORRHS.ORG>
2020-02-13 13:53:54 -0500

    Coding Practice

---
+++ Lesson 3 Variables/app.js
@@ -0,0 +1,39 @@
+// const add = 7 + 3;
+// const sub = 7 - 3;
+// const mult = 7 * 3;
+// const div = 7 / 3;
+// const paren = (83 + 92 + 86 + 94) / 4;
+
+// console.log(add, sub, mult, div, paren)
+
+// let score = 0
+// console.log('Current Score: ' + score) 
+
+// let numLikes = 7
+// console.log('So far ' + numLikes + ' people like this page')
+
+// let score = 0;
+// let lives = 3;
+// console.log('You have ' + lives + ' lives left with score: ' + score);
+
+// let points = 1023;
+// let lives = 3;
+// let totalTime = 45;
+// console.log("Points: " + points);
+// console.log("Lives: " + lives);
+// console.log("Total Time: " + totalTime + " secs");
+// console.log("---------------------------");
+// let totalScore = (points * lives / totalTime);
+// console.log("Your Total Score is: " + totalScore);
+
+// let dice1 = Math.floor(Math.random() * (6 - 1)) + 1;
+// let dice2 = Math.floor(Math.random() * (6 - 1)) + 1;
+// console.log("You rolled a: " + dice1);
+// console.log("and a: " + dice2);
+// let total = (dice1 + dice2);
+// console.log("For a total of: " + total);
+
+let a = 3
+let b = 7
+a = b
+b = a
